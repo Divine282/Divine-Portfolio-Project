2f5e2ba17390ee1cfff23fc3b6ea1e6bb80c10fd
Divine282 <omateydivine@gmail.com>
2026-09-14 00:54:21 +0100

    I Added JavaScript for Interaction

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Portfolio website loaded successfully.");
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function () {
+    console.log("Portfolio interaction detected.");
+});
+
+const year = new Date().getFullYear();
+
+document.querySelector("footer p").textContent =
+    `© ${year} Divine Omatey`;
